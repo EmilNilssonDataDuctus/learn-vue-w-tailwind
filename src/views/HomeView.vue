321ad92ad0e5a35d323cd8c3e2407fb052b7b010
Emil Nilsson <emil.nilsson@dataductus.se>
2023-03-06 17:29:19 +0100

    Add fetch get request to HomeView vue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,18 +4,48 @@
     <div class="relative pt-4 mb-8">
       <input
         v-model="searchQuery"
+        @input="getSearchResults"
         type="text"
         name=""
         id=""
         placeholder="Search for city of state"
         class="w-full px-1 py-2 bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
       />
+      <ul
+        v-if="mapboxSearchResults"
+        class="absolute bg-weather-secondary text-white w-full shadow-md py-2 px-1 top-[66px]"
+      >
+        <li
+          v-for="searchResult in mapboxSearchResults"
+          :key="searchResult.id"
+          class="py-2 cursor-pointer"
+        >
+          {{ searchResult.place_name }}
+        </li>
+      </ul>
     </div>
   </main>
 </template>
 
 <script setup>
 import { ref } from "vue";
+import axios from "axios";
 
 const searchQuery = ref("");
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      const result = await axios.get(``);
+      mapboxSearchResults.value = result.data.features;
+      console.log(mapboxSearchResults.value);
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
 </script>
