fbe73aea35d6dbf33e23d1eb8f611ff2b846127d
Emil Nilsson <emil.nilsson@dataductus.se>
2023-03-06 14:04:18 +0100

    Create markup for search component

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,21 @@
-<script setup></script>
-
 <template>
-  <main>
+  <main class="container text-white">
     <h1>Hello world</h1>
+    <div class="relative pt-4 mb-8">
+      <input
+        v-model="searchQuery"
+        type="text"
+        name=""
+        id=""
+        placeholder="Search for city of state"
+        class="w-full px-1 py-2 bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71]"
+      />
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const searchQuery = ref("");
+</script>
