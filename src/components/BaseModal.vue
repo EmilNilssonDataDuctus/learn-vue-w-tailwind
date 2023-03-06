43cd78997e847559a4b1934f117f3c9a07e0736e
Emil Nilsson <emil.nilsson@dataductus.se>
2023-03-06 13:45:51 +0100

    Add transition to modal

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -1,21 +1,25 @@
 <template>
-  <div
-    v-show="modalActive"
-    class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30"
-  >
+  <Transition name="modal-outer">
     <div
-      v-if="modalActive"
-      class="self-start max-w-screen-md p-4 mt-32 bg-white"
+      v-show="modalActive"
+      class="absolute top-0 left-0 flex justify-center w-full h-screen px-8 bg-black bg-opacity-30"
     >
-      <slot />
-      <button
-        class="px-6 py-2 mt-8 text-white bg-weather-primary"
-        @click="$emit('close-modal')"
-      >
-        Close
-      </button>
+      <Transition name="modal-inner">
+        <div
+          v-if="modalActive"
+          class="self-start max-w-screen-md p-4 mt-32 bg-white"
+        >
+          <slot />
+          <button
+            class="px-6 py-2 mt-8 text-white bg-weather-primary"
+            @click="$emit('close-modal')"
+          >
+            Close
+          </button>
+        </div>
+      </Transition>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <script setup>
@@ -28,4 +32,26 @@ defineProps({
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+.modal-outer-enter-active,
+.modal-outer-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-outer-enter-from,
+.modal-outer-leave-to {
+  opacity: 0;
+}
+.modal-inner-enter-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02) 0.15s;
+}
+.modal-inner-leave-active {
+  transition: all 0.3s cubic-bezier(0.52, 0.02, 0.19, 1.02);
+}
+.modal-inner-enter-from {
+  opacity: 0;
+  transform: scale(0.8);
+}
+.modal-inner-leave-to {
+  transform: scale(0.8);
+}
+</style>
